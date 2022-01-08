528ca9f2b3446d87b5c75e201c7669891742b2a6
holabisii <ajoseholabisi@gmail.com>
2022-01-08 15:29:45 -0500

    included a picture with border radius

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image} from "react-native";
 import { Card } from "react-native-paper";
 
 
@@ -7,6 +7,15 @@ const Home = () => {
 
         return (
                 <Card style =  {styles.mycard}>
+
+                        <Image 
+                        
+                                style={{width: 50, height: 50, borderRadius: 50/2}}
+                                source={{uri: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"}}
+
+                        />
+
+
                         <Text style={{fontSize: 18}}>
                                 This is the home js components
                         </Text>
