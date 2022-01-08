18f168604957ec027515e3801b0810d29fca87ad
holabisii <ajoseholabisi@gmail.com>
2022-01-08 16:46:34 -0500

    iterating over array using map

--- screens/Home.js
+++ screens/Home.js
@@ -5,21 +5,51 @@ import { Card } from "react-native-paper";
 
 const Home = () => {
 
-        return (
-                <Card style =  {styles.mycard}>
+        const data = [ 
+                {id: 1, name: "mukesh", position: "web dev"},
+                {id: 2, name: "rukesh", position: "android dev"},
+                {id: 3, name: "tukesh", position: "ML expert"},
+                {id: 4, name: "sukesh", position: "web dev"},
+        ]
 
-                        <Image 
+        const renderList = data.map((item, key) => {
+
+                return (
+                        <Card style =  {styles.mycard} key= {item.key}>
+
+                                <View style = {styles.cardView}>
+                                        <Image 
+                                        
+                                                style={{width: 50, height: 50, borderRadius: 50/2}}
+                                                source={{uri: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"}}
+
+                                        />
+
+                                        <View style={ styles.textView } >
+                                        
+                                                <Text>
+                                                        {item.name}
+                                                </Text>
+                                                <Text >
+                                                        {item.position}
+                                                </Text>
+
+                                        </View>
+                                
+                                </View>
                         
-                                style={{width: 50, height: 50, borderRadius: 50/2}}
-                                source={{uri: "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGVyc29ufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=600&q=60"}}
+                        </Card>
+                )
+        })
 
-                        />
+        return (
+                <View>
+                       
+                               
+                                {renderList}
 
-
-                        <Text style={{fontSize: 18}}>
-                                This is the home js components
-                        </Text>
-                </Card>
+                
+                </View>
         )
 }
 
@@ -27,6 +57,13 @@ const styles = StyleSheet.create({
         mycard : {
                 margin: 5,
                 padding: 5
+        },
+        cardView : {
+                flexDirection: "row"
+        },
+        textView: {
+                fontSize: 16,
+                margin: 3
         }
 
 })
